8c835a1855ee00631b01f69aa8cf383ac63e1215
Michael Kelly <mkelly@mozilla.com>
2014-12-28 02:57:44 -0800

    Rom info and cpu flags in the sidebar!

--- gamegirl/debugger.py
+++ gamegirl/debugger.py
@@ -5,18 +5,51 @@ import urwid
 from gamegirl.memory import Rom
 
 
+# This code is meant to work, not meant to be pretty.
+# I'll maybe make it better someday.
+
+
+line_divider = urwid.Divider(div_char=u'\u2500')
+blank_divider = urwid.Divider()
+
+
+def block_text(text, align='left', style=None, right=0, left=0):
+    if not isinstance(text, urwid.Text):
+        text = urwid.Text(text, align=align)
+
+    widget = urwid.Padding(text, width=('relative', 100), right=right, left=left)
+    if style:
+        widget = urwid.AttrMap(widget, style)
+
+    return widget
+
+
+def sidebar_title(text):
+    return urwid.Padding(urwid.Text(('sidebar_title', ' {0} '.format(text))), left=2)
+
+
+def sidebar_value(text):
+    return urwid.Padding(urwid.Text(('sidebar_value', ' {0} '.format(text))),
+                         width=('relative', 100), right=2, left=4)
+
+
 class DebuggerInterface(object):
     def __init__(self, cpu):
         self.cpu = cpu
         cpu.debug = True
+        rom = cpu.memory.rom
 
         self.stopped = False
 
         self.palette = [
             ('titlebar', 'black', 'light gray'),
             ('background', 'yellow', 'dark blue'),
-            ('gutter', 'black', 'light gray'),
-            ('register_name', 'black', 'light gray'),
+            ('gutter', 'black', 'dark cyan'),
+            ('register_name', 'white', 'dark cyan'),
+            ('register_value', 'yellow', 'dark blue'),
+            ('sidebar', 'black', 'light gray'),
+            ('sidebar_title', 'white', 'dark cyan'),
+            ('sidebar_value', 'yellow', 'dark blue'),
         ]
 
         self.titlebar = urwid.AttrMap(urwid.Text(('titlebar', 'GameGirl'), align='center'), 'titlebar')
@@ -27,23 +60,80 @@ class DebuggerInterface(object):
 
         # Sidebar
         register_grid = []
-        for register in ('A', 'B', 'C', 'D', 'E', 'F', 'HL', 'SP', 'PC'):
-            text_widget = urwid.Text('_')
+        for register in ('A', 'B', 'C', 'D', 'E', 'F', 'H', 'L', 'SP', 'PC'):
+            text_widget = urwid.Text('_', align='center')
             setattr(self, 'register_' + register, text_widget)
-            register_grid.append(urwid.AttrMap(urwid.Padding(urwid.Text(register, align='center'), width=('relative', 100)), 'register_name'))
-            register_grid.append(text_widget)
+            register_grid.append(block_text(register, align='center', style='register_name'))
+            register_grid.append(block_text(text_widget, style='register_value'))
 
-        self.cpu_heading = urwid.Text('CPU', align='center')
+        flag_grid = []
+        for flag in ('Z', 'N', 'H', 'C'):
+            text_widget = urwid.Text('_', align='center')
+            setattr(self, 'flag_' + flag, text_widget)
+            flag_grid.append(block_text(flag, align='center', style='register_name'))
+            flag_grid.append(block_text(text_widget, style='register_value'))
+
+        if rom.gbc_compatible == Rom.GBC_INCOMPATIBLE:
+            gbc_status = 'Incompatible'
+        elif rom.gbc_compatible == Rom.GBC_COMPATIBLE:
+            gbc_status = 'Compatible'
+        elif rom.gbc_compatible == Rom.GBC_EXCLUSIVE:
+            gbc_status = 'Exclusive'
+        else:
+            gbc_status = 'Unknown'
+
         self.sidebar = urwid.ListBox(urwid.SimpleListWalker([
-            self.cpu_heading,
-            urwid.Divider(div_char='-'),
-            urwid.GridFlow(register_grid, cell_width=5, h_sep=1, v_sep=1, align='center'),
+            urwid.Text('Registers', align='center'),
+            line_divider,
+            urwid.GridFlow(register_grid, cell_width=7, h_sep=1, v_sep=1, align='center'),
+            blank_divider,
+
+            urwid.Text('Flags', align='center'),
+            line_divider,
+            urwid.GridFlow(flag_grid, cell_width=7, h_sep=1, v_sep=1, align='center'),
+            blank_divider,
+
+            urwid.Text('ROM', align='center'),
+            line_divider,
+            sidebar_title('Game'),
+            sidebar_value(rom.title),
+            blank_divider,
+            sidebar_title('Game Code'),
+            sidebar_value(rom.game_code),
+            blank_divider,
+            sidebar_title('Start address'),
+            sidebar_value('${0:04x}'.format(rom.start_address)),
+            blank_divider,
+            sidebar_title('Gameboy Color'),
+            sidebar_value(gbc_status),
+            blank_divider,
+            sidebar_title('Maker code'),
+            sidebar_value(rom.maker_code),
+            blank_divider,
+            sidebar_title('Super Gameboy'),
+            sidebar_value('Yes' if rom.super_gameboy else 'No'),
+            blank_divider,
+            sidebar_title('ROM Size'),
+            sidebar_value(rom.rom_size[1]),
+            blank_divider,
+            sidebar_title('Destination'),
+            sidebar_value('Other' if rom.destination == Rom.DESTINATION_OTHER else 'Japan'),
+            blank_divider,
+            sidebar_title('Mask ROM Version'),
+            sidebar_value(rom.mask_rom_version),
+            blank_divider,
+            sidebar_title('Complement check'),
+            sidebar_value('Passed' if rom.passed_complement_check else 'Failed'),
+            blank_divider,
+            sidebar_title('Checksum'),
+            sidebar_value('${0:04x}'.format(rom.checksum)),
+            blank_divider,
         ]))
 
         # Main layout and loop
         self.top_columns = urwid.Columns([
             ('weight', 2, self.log_list),
-            (27, urwid.LineBox(self.sidebar)),
+            (35, urwid.AttrMap(self.sidebar, 'sidebar')),
         ])
         self.top_frame = urwid.Frame(self.top_columns, header=self.titlebar)
         self.top = urwid.AttrMap(self.top_frame, 'background')
@@ -52,44 +142,25 @@ class DebuggerInterface(object):
         self.update_sidebar()
 
     def start(self):
-        # Print some info about the game we're running.
-        rom = self.cpu.memory.rom
-        self.log('Game: ' + rom.title)
-        self.log('Start address: ${0:04x}'.format(rom.start_address))
-        self.log('Game code: ' + rom.game_code)
-
-        if rom.gbc_compatible == Rom.GBC_INCOMPATIBLE:
-            self.log('Gameboy Color: Incompatible')
-        elif rom.gbc_compatible == Rom.GBC_COMPATIBLE:
-            self.log('Gameboy Color: Compatible')
-        elif rom.gbc_compatible == Rom.GBC_EXCLUSIVE:
-            self.log('Gameboy Color: Exclusive')
-        else:
-            self.log('Gameboy Color: Unknown')
-
-        self.log('Maker code: ' + rom.maker_code)
-        self.log('Super Gameboy: ' + ('Yes' if rom.super_gameboy else 'No'))
-        self.log('ROM Size: ' + rom.rom_size[1])
-        self.log('Destination: ' + ('Other' if rom.destination == Rom.DESTINATION_OTHER else 'Japan'))
-        self.log('Mask ROM Version: {0}'.format(rom.mask_rom_version))
-        self.log('Complement check: ' + ('Passed' if rom.passed_complement_check else 'Failed'))
-        self.log('Checksum: ${0:04x}'.format(rom.checksum))
-        self.log_divider()
-
         self.loop.run()
 
     def update_sidebar(self):
-        for register in ('A', 'B', 'C', 'D', 'E', 'F', 'HL', 'SP', 'PC'):
+        for register in ('A', 'B', 'C', 'D', 'E', 'F', 'H', 'L', 'SP', 'PC'):
+            if len(register) == 1:
+                format_string = '${0:02x}'
+            else:
+                format_string = '${0:04x}'
+
             widget = getattr(self, 'register_' + register)
-            widget.set_text('${0:04x}'.format(getattr(self.cpu, register)))
+            widget.set_text(format_string.format(getattr(self.cpu, register)))
+
+        for flag in ('Z', 'N', 'H', 'C'):
+            widget = getattr(self, 'flag_' + flag)
+            widget.set_text(unicode(getattr(self.cpu, 'flag_' + flag)))
 
     def log(self, text, lineno=''):
         gutter_length = max(9, len(lineno))
-        gutter = urwid.AttrMap(
-            urwid.Padding(
-                urwid.Text(('gutter', lineno), align='right'),
-                width=('relative', 100), right=1),
-            'gutter')
+        gutter = block_text(lineno, style='gutter', right=1, align='right')
 
         self.log_walker.append(urwid.Columns([
             (gutter_length, gutter),
